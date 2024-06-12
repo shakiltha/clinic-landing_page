c358ca8a5d11e00152ecdc54027590f83bbc9ad7
shakil ahmed <shakiltha338@gmail.com>
2024-06-13 00:34:23 +0600

    meet Our doction section for all screen complete

--- src/components/MeetOurDoctors.jsx
+++ src/components/MeetOurDoctors.jsx
@@ -1,7 +1,47 @@
+import doctor1 from "../assets/images/Card 1.png";
+import doctor2 from "../assets/images/Card 2.png";
+import doctor3 from "../assets/images/Card 3.png";
+import ellipse from "../assets/images/Ellipse 413.png";
+import dotBox from "../assets/images/Design element - Circles.png";
 const MeetOurDoctors = () => {
   return (
-    <div>
-      <h2>Meet Our Doctors</h2>
+    <div className="my-4 mx-2 lg:mx-auto lg:mt-[45rem]">
+      <section>
+        <section className="lg:w-96 lg:mx-auto lg:text-center">
+          <h4 className="font-neueHaasDisplayMedium text-2xl lg:text-3xl">
+            Meet Our Great Doctors
+          </h4>
+          <p className="open-sans font-normal">
+            Our team is comprised of highly skilled and compassionate
+            professionals dedicated to your health and well-being.
+          </p>
+        </section>
+        <section className="lg:w-full lg:h-full lg:relative">
+          <img
+            src={dotBox}
+            className="hidden lg:block lg:absolute lg:top-[30rem] xl:top-[30rem] xl:left-40"
+            alt=""
+          />
+          <img
+            src={ellipse}
+            className="hidden lg:block absolute lg:left-[44rem] xl:left-[70rem]"
+            alt=""
+          />
+          <section className="lg:absolute lg:top-16 lg:left-4 xl:left-56">
+            <section className="lg:flex lg:gap-2 lg:justify-center">
+              <section>
+                <img className="mx-auto" src={doctor1} alt="" />
+              </section>
+              <section>
+                <img className="mx-auto" src={doctor2} alt="" />
+              </section>
+              <section>
+                <img className="mx-auto" src={doctor3} alt="" />
+              </section>
+            </section>
+          </section>
+        </section>
+      </section>
     </div>
   );
 };
